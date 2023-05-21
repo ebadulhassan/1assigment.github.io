27fb7d1ed2d6c6446d74da7478deebd1ca5b8a48
ebadulhassan <ebadulhassan967@gmail.com>
2023-05-21 15:27:49 -0700

    1assigment

---
+++ index.js
@@ -0,0 +1,21 @@
+// let  value1 = prompt("1 kg price of chicken is 500 how much do you need");
+// let value2= 0.5*value1
+// document.write("do you need ",value1,"gram","</br>")
+// document.write("you pay this price",value2)
+
+
+
+// let value1 = prompt("1 kg milk is 210  how much do you need ")
+// let value2 = 0.21*value1
+// document.write("do need  milk",value1,"gram","</br>");
+// document.write("you pay this price of " ,value1,"gram","Rs",value2)
+
+
+
+
+
+let user = prompt("the price of pr kg is 500 how much do you need")
+let user2 = 0.5*user
+
+document.write("do you need  ",user,"gram","</br>")
+document.write("you pay this price",user2)
